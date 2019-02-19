a09bdc305101daea19f2db6625d2207912b3e76d
Omar Hussain <omar.hussain@gmail.com>
2019-02-19 06:18:23 -0700

    Updates

---
+++ precache-manifest.8e17b07a3b9121de86b2d54405b11711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4aa34b4b9a56d755be25ebf63f178f9",
+    "url": "/robotfriends/static/media/badrobot.d4aa34b4.woff"
+  },
+  {
+    "revision": "fdaae800c2defa73c763",
+    "url": "/robotfriends/static/js/runtime~main.fdaae800.js"
+  },
+  {
+    "revision": "4f9e7be0a36255886616",
+    "url": "/robotfriends/static/js/main.4f9e7be0.chunk.js"
+  },
+  {
+    "revision": "0155f350101cbdf7c6ab",
+    "url": "/robotfriends/static/js/2.0155f350.chunk.js"
+  },
+  {
+    "revision": "4f9e7be0a36255886616",
+    "url": "/robotfriends/static/css/main.95026e48.chunk.css"
+  },
+  {
+    "revision": "0155f350101cbdf7c6ab",
+    "url": "/robotfriends/static/css/2.4a6fd97c.chunk.css"
+  },
+  {
+    "revision": "2e1da60c9c786bda99cba7e8bc2ff059",
+    "url": "/robotfriends/index.html"
+  }
+];
